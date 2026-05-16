c5baeea3f33cf76e235926e9deef74b57efdf9cb
Hachem S. <o.p.s.hachem@gmail.com>
2026-05-16 00:49:21 -0400

    Submitted Solution for Chapter 01 Ex02-03

---
+++ chapter_01/section_02/ex_01_02_03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float base, height, area;
+    printf("Please enter the base and height of a right triangle: ");
+    scanf("%g%g", &base, &height);
+    area = 0.5 * base * height;
+    printf("The area of the right triangle is: %g\n", area);
+    return 0;
+}
